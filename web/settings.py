68ff9bb462e397f9d24a53ca768af860e88ca98a
Chayan Bawn <chayanbawn@gmail.com>
2019-12-22 19:37:21 +0600

    Django poll tutorial part three.

--- web/settings.py
+++ web/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'so*%)0v2qu(8ggvqyku=vrg1)vzy334#1zy+*s##41_44h7dr('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
